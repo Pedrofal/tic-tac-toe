f378f4fe074efa62817e8deda439454d5769cba2
Pedrofal <pedrofalcao21@gmail.com>
2023-10-24 15:52:11 -0300

    Codigo Final

--- index.js
+++ index.js
@@ -1,54 +1,124 @@
-let box1 = document
-  .getElementById("box1")
-  .addEventListener("click", mark, false);
-let box2 = document
-  .getElementById("box2")
-  .addEventListener("click", mark, false);
-let box3 = document
-  .getElementById("box3")
-  .addEventListener("click", mark, false);
-let box4 = document
-  .getElementById("box4")
-  .addEventListener("click", mark, false);
-let box5 = document
-  .getElementById("box5")
-  .addEventListener("click", mark, false);
-let box6 = document
-  .getElementById("box6")
-  .addEventListener("click", mark, false);
-let box7 = document
-  .getElementById("box7")
-  .addEventListener("click", mark, false);
-let box8 = document
-  .getElementById("box8")
-  .addEventListener("click", mark, false);
-let box9 = document
-  .getElementById("box9")
-  .addEventListener("click", mark, false);
+let currentPlayer;
+let player1;
+let player2;
 
-let player1 = document.getElementById("player1").value;
-let player2 = document.getElementById("player2").value;
-let turnPlayer = document.getElementById("turnPlayer");
-let button = document
-  .getElementById("submit")
-  .addEventListener("click", startGame);
+const turnPlayer = document.getElementById("turnPlayer");
+const button = document.getElementById("submit");
+
+const boxes = Array.from(document.querySelectorAll(".box"));
+const board = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+let gameOver = false; // Variável para rastrear o estado do jogo
+
+function isGameOver() {
+  return gameOver;
+}
 
 function startGame() {
-  turnPlayer.innerText(` Turn: ${player1}`);
+  player1 = document.getElementById("player1").value;
+  player2 = document.getElementById("player2").value;
+  currentPlayer = player1;
+
+  boxes.forEach((box, index) => {
+    box.addEventListener("click", () => mark(index));
+  });
+
+  turnPlayer.innerText = `Turn: ${currentPlayer}`;
 }
 
-function mark(ev) {
-  let target = ev.currentTarget;
+function mark(index) {
+  if (isGameOver()) {
+    return; // O jogo já terminou, não faça nada
+  }
 
-  if (turnPlayer === player1) {
-    target.innerText = "X";
-    target.classList.add("boxClear");
-    target.classList.remove("box");
-    target.removeEventListener("click", mark);
-  } else if (turnPlayer === player2) {
-    target.innerText = "O";
-    target.classList.add("boxClear");
-    target.classList.remove("box");
-    target.removeEventListener("click", mark);
+  const box = boxes[index];
+
+  if (box.innerText === "") {
+    box.innerText = currentPlayer === player1 ? "X" : "O";
+    box.classList.add("boxClear");
+    box.classList.remove("box");
+
+    const row = Math.floor(index / 3);
+    const col = index % 3;
+
+    board[row][col] = currentPlayer;
+
+    if (checkWinner(currentPlayer, row, col)) {
+      // Exibir o último clique antes do popup
+      turnPlayer.innerText = `Winner: ${currentPlayer}`;
+      // Aguardar por um breve momento antes de exibir o popup
+      setTimeout(() => {
+        gameOver = true; // Define o estado do jogo para terminado
+        boxes.forEach((box, index) => {
+          box.removeEventListener("click", () => mark(index));
+        });
+        // Exibir um popup com o nome do jogador vencedor
+        const winner = currentPlayer;
+        const confirmation = window.confirm(`Winner: ${winner}\n\nClick OK to restart the game.`);
+        if (confirmation) {
+          // Reiniciar o jogo
+          resetGame();
+        }
+      }, 500);
+    } else {
+      currentPlayer = currentPlayer === player1 ? player2 : player1;
+      turnPlayer.innerText = `Turn: ${currentPlayer}`;
+    }
   }
 }
+
+function checkWinner(player, row, col) {
+  if (
+    board[row][0] === player &&
+    board[row][1] === player &&
+    board[row][2] === player
+  ) {
+    return true;
+  }
+
+  if (
+    board[0][col] === player &&
+    board[1][col] === player &&
+    board[2][col] === player
+  ) {
+    return true;
+  }
+
+  if (
+    (row === col &&
+      board[0][0] === player &&
+      board[1][1] === player &&
+      board[2][2] === player) ||
+    (row + col === 2 &&
+      board[0][2] === player &&
+      board[1][1] === player &&
+      board[2][0] === player)
+  ) {
+    return true;
+  }
+
+  return false;
+}
+
+function resetGame() {
+  gameOver = false; // Reinicia o estado do jogo
+  board.forEach((row) => row.fill(null)); // Limpa o tabuleiro
+  boxes.forEach((box) => {
+    box.innerText = ""; // Limpa o conteúdo das caixas
+    box.classList.add("box");
+    box.classList.remove("boxClear");
+  });
+  // Limpa os inputs e o turnPlayer
+  document.getElementById("player1").value = "";
+  document.getElementById("player2").value = "";
+  turnPlayer.innerText = "";
+  startGame(); // Inicia um novo jogo
+}
+
+button.addEventListener("click", startGame);
+
+startGame();
